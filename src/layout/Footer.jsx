087fa442fc91849b81c7e2ef0bc42701d9e8d5d8
A-slastikhina <ao.slastikhina@gmail.com>
2023-03-01 12:18:51 +0600

    added correct year

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -3,7 +3,7 @@ function Footer (){
 
         <div className="footer-copyright">
         <div className="container  blue-grey-text text-lighten-5">
-        © {new Date().getFullYear} Copyright Text
+        © {new Date().getFullYear()} Copyright
         <a className="blue-grey-text text-lighten-4 right" href="#!">Repo</a>
         </div>
         </div>
